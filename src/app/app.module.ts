95d4232b79901d74373a5bd4030924f08b9ee250
unknown <vvauban@gmail.com>
2020-07-25 03:49:48 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SanitizeHtmlPipe } from './sanitizehtml.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    SanitizeHtmlPipe,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SanitizeHtmlPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/parcel.model.ts
@@ -0,0 +1,5 @@
+export interface Parcel {
+    id: string;
+    weight: string;
+    destination: string;
+}
